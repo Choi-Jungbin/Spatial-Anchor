afee7e7ea8ab68cdff0ac83728bcb0447ab65edc
JB <wjdqls6704@naver.com>
2023-10-14 02:46:56 +0900

    feat: 적 애니메이션 수정

--- Assets/Scripts/FPS/Enemy.cs
+++ Assets/Scripts/FPS/Enemy.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private enum enemyState
-    {
-        Walk,
-        Dead,
-        Search,
-        attack
-
-    }
-
     private Rigidbody[] ragdollRigidbodies;
-    private enemyState currentState = enemyState.Search;
     private Animator animator;
 
     // Start is called before the first frame update
@@ -22,7 +12,6 @@ public class Enemy : MonoBehaviour
     {
         ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
         animator = gameObject.GetComponent<Animator>();
-        InvokeRepeating("setAnimator", 0, 5);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -30,18 +19,6 @@ public class Enemy : MonoBehaviour
         if (collision.gameObject.CompareTag("bullet_head"))
         {
             animator.SetTrigger("bullet");
-        }
-    }
-
-    void setAnimator()
-    {
-        if (animator.GetBool("act1"))
-        {
-            animator.SetBool("act1", false);
-        }
-        else
-        {
-            animator.SetBool("act1", true);
         }
     }
 }
